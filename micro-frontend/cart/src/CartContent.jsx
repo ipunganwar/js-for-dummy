f6f8e6ce09f021ee3a99ef7801afe5bfa9028fd8
ipunganwar <ipung.anwar1@gmail.com>
2022-02-14 03:15:11 +0700

    add cart service for UI app

---
+++ micro-frontend/cart/src/CartContent.jsx
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react'
+
+import MiniCart from './MiniCart'
+import Login from './Login'
+import { jwt, login } from './cart'
+
+export default function CartContent() {
+  const [token, setToken] = useState("")
+  useEffect(() => {
+    return jwt.subscribe(val => setToken(val ?? ''))
+  }, [])
+
+  return (
+    <div>
+      <div>JWT: {token}</div>
+      <Login />
+      <MiniCart />
+    </div>
+  )
+  
+}
