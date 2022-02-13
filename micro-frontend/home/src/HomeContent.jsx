001a50ce3725413a35e369672549fa8adedadbd8
ipunganwar <ipung.anwar1@gmail.com>
2022-02-13 14:34:46 +0700

    handling product query in pdp app

--- micro-frontend/home/src/HomeContent.jsx
+++ micro-frontend/home/src/HomeContent.jsx
@@ -9,10 +9,17 @@ export default function HomeContent() {
   }, [])
 
   return (
-    <div className="my-10 grid grid-cols-4 gap-5">
+    <div className="grid grid-cols-4 gap-5">
       {products.map((product) => (
         <div key={product.id}>
           <img src={product.image} alt={product.name} />
+          <div className="flex">
+            <div className="flex-grow font-bold">
+              <a>{product.name}</a>
+            </div>
+            <div className="flex-end"> {currency.format(product.price)} </div>
+            <div className="text-sm mt-4">{product.description}</div>
+          </div>
         </div>
       ))}
     </div>
