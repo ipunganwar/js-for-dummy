3d5f5068ad3a45797c49651252a50ed7b7b4d2ac
ipunganwar <ipung.anwar1@gmail.com>
2022-02-28 08:13:25 +0700

    add event detail

--- nextjs/nextjs-basic-routing-project/pages/events/[eventId].js
+++ nextjs/nextjs-basic-routing-project/pages/events/[eventId].js
@@ -1,7 +1,34 @@
+import { useRouter } from 'next/router'
+import { Fragment } from 'react'
+
+import { getEventById } from '../../data/dummy-data'
+import EventSummary from '../../components/event-detail/event-summary'
+import EventLogistic from '../../components/event-detail/event-logistics'
+import EventContent from '../../components/event-detail/event-content'
+
 export default function EventDetailPage () {
+  const router = useRouter()
+
+  const eventId = router.query.eventId
+  const event = getEventById(eventId)
+
+  console.log(event)
+  if (!event) {
+    return (<p>No Event found !</p>)
+  }
+
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistic
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   )
 }
