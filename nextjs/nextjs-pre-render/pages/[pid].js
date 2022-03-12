04f8bc08531d9d20ccef798a16dd53d16726ff26
ipunganwar <ipung.anwar1@gmail.com>
2022-03-12 14:42:44 +0700

    add getStaticPaths

--- nextjs/nextjs-pre-render/pages/[pid].js
+++ nextjs/nextjs-pre-render/pages/[pid].js
@@ -15,16 +15,25 @@ export default function ProductDetailPage (props) {
 
 export async function getStaticProps (context) {
   const { params } = context
-  console.log(params)
-  const productId = params.id
+  const productId = params.pid
   const pathFile = path.join(process.cwd(), 'data', 'dummy-backend.json')
   const jsonData = await fs.readFile(pathFile)
   const data = JSON.parse(jsonData)
   const productFind = data.products.find(product => product.id === productId)
-
   return {
     props: {
       loadedProduct: productFind
     }
+  }
+}
+
+export async function getStaticPaths () {
+  return {
+    paths: [
+      { params: { pid: 'p1' } },
+      { params: { pid: 'p2' } },
+      { params: { pid: 'p3' } },
+    ],
+    fallback: false,
   }
 }
