7b118c80bed746c403e75616ab7b203e837ea2f2
ipunganwar <ipung.anwar1@gmail.com>
2022-03-12 23:55:04 +0700

    init getStaticProps

---
+++ nextjs/nextjs-pre-render/pages/user-profile.js
@@ -0,0 +1,11 @@
+export default function UserProfilePage (props) {
+  return <h1>{props.username}</h1>
+}
+
+export async function getServerSideProps (context) {
+  const { params, req, res } = context
+  console.log(req)
+  return {
+    props: { username: 'Max'}
+  }
+}
